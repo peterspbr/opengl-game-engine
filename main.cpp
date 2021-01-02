0ecc419206cf471e6b8ab7ad9b340f3d7fcfcb07
peterspbr <peter.spbr2002@gmail.com>
2021-01-02 16:27:55 -0300

    Texture

--- main.cpp
+++ main.cpp
@@ -120,20 +120,22 @@ void createTriangle()
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // texture 1
-    // ---------
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
+
     // set the texture wrapping parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
     // set texture filtering parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
     unsigned char *data = stbi_load("resources/textures/pine.jpg", &width, &height, &nrChannels, 0);
+
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -143,8 +145,8 @@ void createTriangle()
     {
         std::cout << "Failed to load texture" << std::endl;
     }
-    stbi_image_free(data);
 
+    stbi_image_free(data);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
